0b7869257be05e72a3df7dde2f4e002b082efac2
Jacob Lauxman <jlauxman@gmail.com>
2023-01-30 15:49:30 -0700

    re-added migrations SCHEMA production values and table presets after updating database tables

--- migrations/versions/20230130_150129_created_wishlists_association_updated_.py
+++ migrations/versions/20230130_150129_created_wishlists_association_updated_.py
@@ -1,13 +1,16 @@
 """created_wishlists_association_updated_user_to_dict
 
 Revision ID: 77ab50d857ea
-Revises: 
+Revises:
 Create Date: 2023-01-30 15:01:29.320500
 
 """
 from alembic import op
 import sqlalchemy as sa
 
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 # revision identifiers, used by Alembic.
 revision = '77ab50d857ea'
@@ -93,6 +96,24 @@ def upgrade():
     sa.ForeignKeyConstraint(['order_id'], ['orders.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+
+
+    if environment == "production":
+        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
+    if environment == "production":
+        op.execute(f"ALTER TABLE items SET SCHEMA {SCHEMA};")
+    if environment == "production":
+        op.execute(f"ALTER TABLE carts SET SCHEMA {SCHEMA};")
+    if environment == "production":
+        op.execute(f"ALTER TABLE orders SET SCHEMA {SCHEMA};")
+    if environment == "production":
+        op.execute(f"ALTER TABLE reviews SET SCHEMA {SCHEMA};")
+    if environment == "production":
+        op.execute(f"ALTER TABLE carts_items SET SCHEMA {SCHEMA};")
+    if environment == "production":
+        op.execute(f"ALTER TABLE orders_items SET SCHEMA {SCHEMA};")
+    if environment == "production":
+        op.execute(f"ALTER TABLE wishlists SET SCHEMA {SCHEMA};")
     # ### end Alembic commands ###
 
 
